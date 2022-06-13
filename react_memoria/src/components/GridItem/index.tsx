25fa43a9426181fb50db315ff7bffd63370b4ddf
gabriel1og <gogomide.go@gmail.com>
2022-06-13 15:28:14 -0300

    visaualizacao icones

--- react_memoria/src/components/GridItem/index.tsx
+++ react_memoria/src/components/GridItem/index.tsx
@@ -1,5 +1,7 @@
 import { GridItemType } from '../../types/GridItemType'
 import * as C from './styles'
+import b7Svg from '../../svgs/b7.svg'
+import { Items } from '../../data/Items'
 
 type Props = {
     item: GridItemType,
@@ -9,7 +11,12 @@ type Props = {
 export const GridItem= ({ item, onClick }: Props) => {
     return (
         <C.Container onClick={onClick}>
-            ...
+            {item.permanentShown === false && item.shown === false &&
+                <C.Icon src={b7Svg} alt='' />
+            }
+            {(item.permanentShown || item.shown) && item.item !== null &&
+                <C.Icon src={Items[item.item].icon} alt='' />
+            }
         </C.Container>
     )
 }
--- react_memoria/src/components/GridItem/styles.ts
+++ react_memoria/src/components/GridItem/styles.ts
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+   background-color: red; 
+`;
+
+export const Icon = styled.img`
+
+`;
